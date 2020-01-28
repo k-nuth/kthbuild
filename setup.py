0626b93c544306b14ac87ddbd7dcf697e3612b7c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-01-28 00:25:29 -0300

    fixes get_builder

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 __title__ = "kthbuild"
 __summary__ = "Knuth node build tools"
 __uri__ = "https://github.com/k-nuth/kthbuild"
-__version__ = "0.0.22"
+__version__ = "0.0.23"
 __author__ = "Fernando Pelliccioni"
 __email__ = "fpelliccioni@gmail.com"
 __license__ = "MIT"
